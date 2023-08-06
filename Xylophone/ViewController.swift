cfc747e6a42800ad5ef3037d14f3bff42c68f789
Burak Can Karasel <99825584+burakkarasel@users.noreply.github.com>
2023-08-06 13:25:31 +0300

    Made App functional to play sound

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,24 +18,50 @@ class ViewController: UIViewController {
     }
     
     @IBAction func cTapped(_ sender: UIButton) {
+        playSound(fileName: "C")
     }
     
     @IBAction func dTapped(_ sender: UIButton) {
+        playSound(fileName: "D")
     }
     
     @IBAction func eTapped(_ sender: UIButton) {
+        playSound(fileName: "E")
     }
     
     @IBAction func fTapped(_ sender: UIButton) {
+        playSound(fileName: "F")
     }
     
     @IBAction func gTapped(_ sender: UIButton) {
+        playSound(fileName: "G")
     }
     
     @IBAction func aTapped(_ sender: UIButton) {
+        playSound(fileName: "A")
     }
     
     @IBAction func bTapped(_ sender: UIButton) {
+        playSound(fileName: "B")
+    }
+    
+    func playSound(fileName : String) {
+        // Build the url for the sound file
+        guard let url = Bundle.main.url(forResource: "Sounds/\(fileName)", withExtension: "wav") else {return}
+        do {
+            // we use the singleton AVAudioSession instance through our app lifesycle
+            // here we set the session as playback so we can still play sounds while iOS app is in silence mode
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            // here we activate the AVAudioSession with categories we specified by setting it true
+            try AVAudioSession.sharedInstance().setActive(true)
+            // set AVAudioPlayer with the url which we built
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            // play the sound
+            player?.play()
+        }catch let error {
+            // check for error
+            print(error.localizedDescription)
+        }
     }
 }
 
